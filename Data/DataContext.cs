6c4fb80d2e1149b6dc78ebfe45e8f5f4ea3fceaf
chrisAndriotti <andriottichris@gmail.com>
2023-07-23 17:33:49 -0300

    Configuração da base de dados

---
+++ Data/DataContext.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CentralizadorExames.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CentralizadorExames.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbSet<Acompanhamento> Acompanhamento {get; set;}
+        public DbSet<Cargo> Cargo {get; set;}
+        public DbSet<Exame> Exame {get; set;}
+        public DbSet<Paciente> Paciente {get; set;}
+        public DbSet<Profissional> Profissional {get; set;}
+
+
+        public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
