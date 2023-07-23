afadf970ae00980269608e10449000ebbb34b576
chrisAndriotti <andriottichris@gmail.com>
2023-07-23 16:46:42 -0300

    configuração inicial

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using CentralizadorExames.Data;
+using CentralizadorExames.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            ); 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ILeitorArquivoService, LeitorArquivoService>();
+builder.Services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "Centralizador de Exames V1");
+      c.RoutePrefix = String.Empty;
+});
+
+app.Run();
