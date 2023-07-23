aa87bea0ad4d5dec006d065dcf533aa4bfe288a0
chrisAndriotti <andriottichris@gmail.com>
2023-07-23 16:52:18 -0300

    Arquivos de migrations

---
+++ Migrations/20230722223136_InitialCreate.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CentralizadorExames.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cargo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cargo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paciente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NomeResponsavel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TelefoneReponsavel = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paciente", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Acompanhamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PacienteId = table.Column<int>(type: "int", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    DataAbertura = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFechamento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Acompanhamento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Acompanhamento_Paciente_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Paciente",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exame",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AcompanhamentoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exame", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exame_Acompanhamento_AcompanhamentoId",
+                        column: x => x.AcompanhamentoId,
+                        principalTable: "Acompanhamento",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profissional",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CargoId = table.Column<int>(type: "int", nullable: true),
+                    Registro = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AcompanhamentoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profissional", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profissional_Acompanhamento_AcompanhamentoId",
+                        column: x => x.AcompanhamentoId,
+                        principalTable: "Acompanhamento",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Profissional_Cargo_CargoId",
+                        column: x => x.CargoId,
+                        principalTable: "Cargo",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Acompanhamento_PacienteId",
+                table: "Acompanhamento",
+                column: "PacienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exame_AcompanhamentoId",
+                table: "Exame",
+                column: "AcompanhamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profissional_AcompanhamentoId",
+                table: "Profissional",
+                column: "AcompanhamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profissional_CargoId",
+                table: "Profissional",
+                column: "CargoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exame");
+
+            migrationBuilder.DropTable(
+                name: "Profissional");
+
+            migrationBuilder.DropTable(
+                name: "Acompanhamento");
+
+            migrationBuilder.DropTable(
+                name: "Cargo");
+
+            migrationBuilder.DropTable(
+                name: "Paciente");
+        }
+    }
+}
