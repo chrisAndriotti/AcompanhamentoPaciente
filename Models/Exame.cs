e33d3f20860e1b1026f577383d4b3a8bf70d8e3d
chrisAndriotti <andriottichris@gmail.com>
2023-07-30 04:53:51 -0300

    Entidade do Exame

--- Models/Exame.cs
+++ Models/Exame.cs
@@ -5,6 +5,6 @@ namespace AcompanhamentoPaciente.Models
       public int Id { get; set; }
       public string? Nome { get; set; }
       public string? Descricao { get; set; }
-      public DateTime Data { get; set; }
+      public DateTime Data { get; set; } 
    }
 }
